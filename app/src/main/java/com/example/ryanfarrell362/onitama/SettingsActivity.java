5e25248b3d9ea9cd7cee236b9b519636dde0e7ab
SuperSquirrel51 <SuperSquirrel51@users.noreply.github.com>
2018-01-12 18:59:47 -0500

    More tests

--- app/src/main/java/com/example/ryanfarrell362/onitama/SettingsActivity.java
+++ app/src/main/java/com/example/ryanfarrell362/onitama/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.ryanfarrell362.onitama;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Intent intent = getIntent();
     }
 }
