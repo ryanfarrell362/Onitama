ab52ae72c8f45d33d10f1895e7d8db93f6c30680
SuperSquirrel51 <SuperSquirrel51@users.noreply.github.com>
2018-01-12 18:23:43 -0500

    Test

--- app/src/main/java/com/example/ryanfarrell362/onitama/SettingsActivity.java
+++ app/src/main/java/com/example/ryanfarrell362/onitama/SettingsActivity.java
@@ -1,8 +1,13 @@
 package com.example.ryanfarrell362.onitama;
 
-/**
- * Created by ryan.farrell362 on 1/11/2018.
- */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity {
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
