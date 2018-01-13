0b1c908cfcf3803b8fba8656c548f046ce2d6b5b
SuperSquirrel51 <SuperSquirrel51@users.noreply.github.com>
2018-01-12 19:01:53 -0500

    Slightly refined intent call

--- app/src/main/java/com/example/ryanfarrell362/onitama/SettingsActivity.java
+++ app/src/main/java/com/example/ryanfarrell362/onitama/SettingsActivity.java
@@ -10,7 +10,5 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        Intent intent = getIntent();
     }
 }
